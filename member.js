1fec18e242e3b798e7933c37f822cad9f4bb9a5d
Prita Dey <pritadey@microsoft.com>
2025-03-12 13:13:14 +0530

    js added

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    return {
+        name: "John Doe",
+        age: 25,
+        skills: [
+            { name: "JavaScript", level: "Advanced" },
+            { name: "Node.js", level: "Advanced" },
+            { name: "HTML", level: "Intermediate" },
+            { name: "CSS", level: "Intermediate" },
+        ]
+    };
+}
